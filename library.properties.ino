edfb66673b8d8631fc9ffa154a619fe8585bf6ee
Yuhan Cruz <195582583+yuhan68@users.noreply.github.com>
2025-01-21 09:36:41 -0500

    Changing comments and information
    Signed-off-by: Yuhan Cruz <195582583+yuhan68@users.noreply.github.com>

--- library.properties.ino
+++ library.properties.ino
@@ -7,12 +7,12 @@ maintainer=Yuhan Cruz, ymaxcruz26@gmail.com
 sentence=Provides a predefined set of frequencies for musical notes from A0 to C8, 
 and adds functionalities for piano coding.
 
-paragraph=All the pianos notes are defined by note and octave, e.g. (c3), while sharps are noted by a note, sharp, 
-and then an octave e.g. (c_sharp3). Furthermore, there is a function that sets the bpm for the duration functions 
-e.g. (whole()) that allows tunes to be coded and played and different speeds.
+paragraph=All the piano's notes are defined by note and octave, e.g. (c3), while sharps are noted by a note, sharp, 
+and then an octave, e.g. (c_sharp3). Furthermore, there is a function that sets the bpm for the duration functions, 
+e.g. (whole()), that allows tunes to be coded and played at different speeds.
 
 category - Other
 url - https://github.com/yuhan68/Yuhan68-Piano_Notes-Library
-architectures=*
+architectures=avr
 */
 
